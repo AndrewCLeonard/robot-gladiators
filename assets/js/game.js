ac0a3196b56d3a37f676cf5c7525979a8c397a5f
Andrew Leonard <AndrewCLeonard@fastmail.com>
2022-07-12 15:48:01 -0700

    WIP robot name setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,6 @@
 // DOM Elements
-const fightStage = document.getElementById("fightStage");
+
+const fightStageId = document.getElementById("fightStage");
 
 // Bindings
 var onLoadRobotNameValue = document.getElementById("robotName").value;
@@ -10,19 +11,48 @@ console.log(`onLoadRobotNameValue is empty? ${"" === onLoadRobotNameValue}`);
 
 // functions
 function displayRobotName() {
-	fightStage.style.visibility = "visible";
+	fightStageId.style.visibility = "visible";
 }
 
 const nameFunctions = () => {
 	event.preventDefault();
-	const robotNameValueOnClick = document.getElementById("robotName").value;
-	console.log(`robotNameValueOnClick = ${robotNameValueOnClick}`);
 
-	console.log("test");
+	// local vars
+	const robotNameValueOnClick = document.getElementById("robotName").value;
+
+	console.log(`robotNameValueOnClick = ${robotNameValueOnClick}`);
+	console.log(document.getElementById("robotName").innerHTML);
+
+	//
 	if (robotNameValueOnClick) {
-		fightStage.style.visibility = "visible";
+		// displayRobotName();
+		console.log(fightStageId);
+
+		function appendRobotName() {
+			const para = document.createElement("p");
+			const helloTextForRobotName = document.createTextNode(robotNameValueOnClick);
+
+			para.appendChild(helloTextForRobotName);
+			document.body.appendChild(para);
+		}
+		appendRobotName();
 	}
 
 	// displayRobotName();
 	// showFightStage();
 };
+
+// Fight Variables
+playerCurrency = 10;
+playerHealth = 10;
+
+// enemy robot collection
+function Enemy(name, hitPoints) {
+	this.name = name;
+	this.hitPoints = hitPoints;
+}
+
+const enemy1 = new Enemy("enemy1", 10);
+const enemy2 = new Enemy("enemy2", 10);
+
+const enemyCollection = [enemy1, enemy2];
