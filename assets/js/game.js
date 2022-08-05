ad9931343bc9752a909d4e9300752e2b331aa5b0
Andrew Leonard <AndrewCLeonard@fastmail.com>
2022-08-04 17:28:36 -0700

    messy, but it works

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,8 @@
-const paragraphEl = document.createElement("p");
+const h2El = document.createElement("h2");
 
 // will contain robot name when "submit" is clicked
 let nameSubmission = "";
+let booleanWinner = "";
 
 const robotNameSubmitBtn = document.getElementById("robotNameSubmitBtn");
 robotNameSubmitBtn.addEventListener(
@@ -11,7 +12,7 @@ robotNameSubmitBtn.addEventListener(
 	function startGame(e) {
 		e.preventDefault;
 		nameSubmission = document.getElementById("robotName").value.trim(); // robotName = submission
-		displayRobotReadyMsg(); // `style.visibility = "visible"` of...
+		displayRobotReadyMsg(); // make `style.visibility = "visible"` for `RobotReadyMsg()`
 		RobotReadyMsg(); // Battle message & attack button
 	}
 );
@@ -22,21 +23,42 @@ function displayRobotReadyMsg() {
 }
 
 function RobotReadyMsg() {
-	// get robot's name from form & create message to be appended to DOM after robot name is submitted
+	// create battle message
 	let msg = `It's time to battle, ${nameSubmission}!`;
-	// create a <p> element and append it to the `id=fightStage` <div> element
+	// put the message in a text node
 	const battleMessage = document.createTextNode(msg);
-	paragraphEl.appendChild(battleMessage);
-
+	// append it to id='readyAnnouncement'
 	const readyAnnouncementId = document.getElementById("readyAnnouncement");
-	readyAnnouncementId.appendChild(paragraphEl);
+	readyAnnouncementId.appendChild(battleMessage);
+	// return nameSubmission;
+}
+
+// attack function logic
+function chooseWinner(e) {
+	e.preventDefault;
+	// message options to display
+	let winMessage = `🏆${nameSubmission} wins!🏆`;
+	let loseMessage = `☠${nameSubmission} loses!☠`;
+
+	// random number to determine win or loss
+	booleanWinner = Math.random() > 0.5 ? winMessage : loseMessage;
+	displayBattleResult();
 }
 
 const attackBtn = document.getElementById("attackBtn");
-attackBtn.addEventListener("click", function chooseWinner(e) {
-	e.preventDefault;
-	booleanWinner = Math.random() > 0.5 ? winMessage : loseMessage;
-});
+attackBtn.addEventListener("click", chooseWinner);
+
+function displayBattleResult() {
+	console.log(booleanWinner);
+	const battleResultId = document.getElementById("battleResult");
+	battleResultId.style.visibility = "visible";
+
+	const battleResultText = document.createTextNode(booleanWinner);
+	battleResultId.appendChild(battleResultText);
+
+	const lineBreak = document.createElement("br");
+	battleResultId.appendChild(lineBreak);
+}
 
 // Fight Variables
 playerCurrency = 10;
